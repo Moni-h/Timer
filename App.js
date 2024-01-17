d99556607b9e1f9e052a62efe605ca8a74a1a809
Moni-h <monika.hambardzumyan@gmail.com>
2024-01-17 18:22:40 +0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import { colors } from './src/utils/colors';
+import { Focus } from './src/feautures/Focus';
+import { Timer } from './src/feautures/Timer';
+
+export default function App() {
+  const [currentSubject, setCurrentSubject] = useState(null);
+  return (
+    <SafeAreaView style={styles.container}>
+      {!currentSubject ? (
+        <Focus addSubject={setCurrentSubject} />
+      ) : (
+        <Timer 
+          focusSubject={currentSubject}
+          onTimerEnd={() => {}}
+          clearSubject={() => {}}
+        />
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+});
---
+++ ios/Pods/Target Support Files/Pods-UdemyFitstProject-UdemyFitstProjectTests/Pods-UdemyFitstProject-UdemyFitstProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UdemyFitstProject_UdemyFitstProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_UdemyFitstProject_UdemyFitstProjectTests
+@end
